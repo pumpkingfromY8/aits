3e8565b7acb561ec76b28f850e22190c91028c17
PUMPKINGfromY8 <136235484+pumpkingfromY8@users.noreply.github.com>
2025-04-30 16:23:25 +0800

    dami diko ma bilang
    with pages separated to optimized

---
+++ assets/js/page_route.js
@@ -0,0 +1,31 @@
+// page_route.js
+function loadPage(url) {
+    fetch(url)
+      .then(res => res.text())
+      .then(html => {
+        document.getElementById('app').innerHTML = html;
+      });
+  }
+  
+  page('/', () => loadPage('/pages/home.html'));
+  page('/home', () => loadPage('/pages/home.html'));
+  page('/programs', () => loadPage('/pages/programs.html'));
+  page('/centers', () => loadPage('/pages/centers.html'));
+  page('/contact', () => loadPage('/pages/contact.html'));
+  page({ hashbang: true }); // 🔁 Enable hash routing
+  
+  // auto scroll top
+  page('*', (ctx, next) => {
+    document.getElementById('top').scrollIntoView({ behavior: 'smooth' });
+    next();
+  });
+  
+  
+  
+  document.querySelectorAll('.navbar-link').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      page(this.getAttribute('href'));
+    });
+  });
+  
